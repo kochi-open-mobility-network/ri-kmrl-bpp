d5875dcbe2743819db9e6f22b4b2f477121eef0a
techframewirk <65583767+techframewirk@users.noreply.github.com>
2021-10-26 16:25:02 +0530

    Config values can be taken from env variables

--- README.md
+++ README.md
@@ -4,4 +4,10 @@
 docker build . -t metro-bpp
 
 ### Docker run
-docker run -p 8000:8000 --name metro --env MAPS_KEY=<<key>> metro-bpp
+docker run -p 8000:8000 --name metro --env MAPS_KEY=< key > metro-bpp
+
+#### Other environment variables and default values if no value is passed :
+    bpp_id: 'metro-bpp'
+    bpp_uri: 'https://metro-bpp.com/'
+    registry_url: 'https://beckn-one.succinct.in/subscribers/'
+    unique_key_id: 'key1'
--- config/config.ts
+++ config/config.ts
@@ -4,9 +4,9 @@ export const config = {
     USE_TIME_THRESHOLD : false, //Use time to determine close stops
     USE_MAPS_API : true, //Use Google maps API to determine close stops
     MAX_STATIONS : 2, //Maximum number of nearby stops to return for a location
-    bpp_id: 'metro-bpp',
-    bpp_uri: 'https://metro-bpp.com/',
-    registry_url: 'https://beckn-one.succinct.in/subscribers/',
-    unique_key_id: 'key1',
+    bpp_id: process.env.bpp_id || 'metro-bpp',
+    bpp_uri: process.env.bpp_uri || 'https://metro-bpp.com/',
+    registry_url: process.env.registry_url || 'https://beckn-one.succinct.in/subscribers/',
+    unique_key_id: process.env.unique_key_id || 'key1',
     auth: true
 }
